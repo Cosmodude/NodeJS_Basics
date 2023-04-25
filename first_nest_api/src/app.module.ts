6f7f8bdfbc8bef03be468d836b449fc550e12c7e
Hackdady <lenskii.vm19@physics.msu.ru>
2023-04-25 13:23:11 +0900

    g resource example

--- first_nest_api/src/app.module.ts
+++ first_nest_api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NominatorPoolsModule } from './nominator_pools/nominator_pools.module';
+import { ExampleModule } from './example/example.module';
 
 @Module({
-  imports: [NominatorPoolsModule],
+  imports: [NominatorPoolsModule, ExampleModule],
   controllers: [AppController],
   providers: [AppService],
 })
