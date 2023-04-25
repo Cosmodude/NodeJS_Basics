524ae210ae7c5a898d0c12545b243598523460f4
Hackdady <lenskii.vm19@physics.msu.ru>
2023-04-25 13:23:29 +0900

    endpoints created

--- first_nest_api/src/nominator_pools/nominator_pools.controller.ts
+++ first_nest_api/src/nominator_pools/nominator_pools.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query, Body } from '@nestjs/common';
+import { CreatePoolDto } from 'src/nominator_pools/dto/create-pool.dto';
 
 @Controller('nominator-pools')
-export class NominatorPoolsController {}
+export class NominatorPoolsController {
+    @Get()
+    getPools() {
+        return  [];
+    }
+    @Get('/bytype')
+    getActivePools(@Query('type') type: string) {
+        return  [{"type": type},];
+    }
+    @Get(':id')
+    getPool(@Param('id') id: string) {
+        return  {id,};
+    }
+    @Post()
+    createPool(@Body() pool: CreatePoolDto) {
+        return pool.address;
+    }
+
+}
