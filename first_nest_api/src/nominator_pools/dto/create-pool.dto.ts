134651845a36892f5d75205e87ca14625dd0c39c
Hackdady <lenskii.vm19@physics.msu.ru>
2023-04-28 11:44:16 +0900

    validators added

--- first_nest_api/src/nominator_pools/dto/create-pool.dto.ts
+++ first_nest_api/src/nominator_pools/dto/create-pool.dto.ts
@@ -1,8 +1,15 @@
+import { MinLength, IsEnum } from "class-validator";
+
 export class CreatePoolDto {
     address: string;
+
     totalStake: number;
+
+    @MinLength(3)
     name: string;
     profitShare: number;
+
+    @IsEnum(['custodial', 'non-custodial'],{message:"use proper custodity type"})
     type: string;
     
 
