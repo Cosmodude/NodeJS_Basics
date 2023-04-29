d18a0a72d40ecbb652e0ff10b00640beaf5986f0
Hackdady <lenskii.vm19@physics.msu.ru>
2023-04-29 21:34:54 +0900

    looking at the request body

--- first_nest_api/src/wallet/wallet.guard.ts
+++ first_nest_api/src/wallet/wallet.guard.ts
@@ -12,6 +12,7 @@ export class WalletGuard implements CanActivate {
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
     //console.log(env.Admin_Wallet);
+    //console.log(request);
     //const isAdmin = (request.user.wallet === env.Admin_Wallet) ;
     return true;
   }
