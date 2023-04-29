3c5863f719798b445fcc652c336c4b32686ec1d3
Hackdady <lenskii.vm19@physics.msu.ru>
2023-04-29 22:30:08 +0900

    module imports modified

--- first_nest_api/src/app.module.ts
+++ first_nest_api/src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NominatorPoolsModule } from './nominator_pools/nominator_pools.module';
 import { ExampleModule } from './example/example.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pool } from './nominator_pools/pool.entity';
 
 @Module({
-  imports: [NominatorPoolsModule, ExampleModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'nominator-pools',
+      entities: [Pool],
+      autoLoadEntities: true,
+      synchronize: true, //Indicates if database schema should be auto created on every application launch.
+    }),
+    NominatorPoolsModule,
+    ExampleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
