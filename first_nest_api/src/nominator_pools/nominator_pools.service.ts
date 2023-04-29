7eb6cfbfe4d746a38151d0f6151ae70a4bd4dfec
Hackdady <lenskii.vm19@physics.msu.ru>
2023-04-29 22:30:59 +0900

    service upgraded

--- first_nest_api/src/nominator_pools/nominator_pools.service.ts
+++ first_nest_api/src/nominator_pools/nominator_pools.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Pool } from './user.entity';
 import { CreatePoolDto } from './dto/create-pool.dto';
-
 @Injectable()  // shows that you can automatiacally inject it by providing as param into constructor
 export class NominatorPoolsService {
+    // for DBs
+    constructor(
+        @InjectRepository(Pool)
+        private poolsRepository: Repository<Pool>,
+        private dataSource: DataSource,
+      ) {}
+    
+    async createMany(pools: Pool[]) {
+        await this.dataSource.transaction(async manager => {
+            await manager.save(pools[0]);
+            await manager.save(pools[1]);
+        });
+    }
+      
+      findAll(): Promise<Pool[]> {
+        return this.poolsRepository.find();
+    }
+    
     private pools = [
         { id: 0, address: "0x1", totalStake: 1e6, name: "First Pool", profitShare: 60,type: "custodial"},
         { id: 1, address: "0x2", totalStake: 5e5, name: "TEB Pool", profitShare: 90, type: "non-custodial"},
