8fad1f2dbac22d33c06d9922b9da6c552d13ecea
Hackdady <lenskii.vm19@physics.msu.ru>
2023-04-29 22:30:37 +0900

    pool module imports and providers added

--- first_nest_api/src/nominator_pools/nominator_pools.module.ts
+++ first_nest_api/src/nominator_pools/nominator_pools.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { NominatorPoolsController } from './nominator_pools.controller';
 import { NominatorPoolsService } from './nominator_pools.service';
-
+import { Pool } from './pool.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([Pool])],
   controllers: [NominatorPoolsController],
-  providers: [NominatorPoolsService]
+  providers: [NominatorPoolsService],
 })
 export class NominatorPoolsModule {}
