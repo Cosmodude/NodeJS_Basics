05a3df4dcb8b47412f98014b0662537455f7b573
Hackdady <lenskii.vm19@physics.msu.ru>
2023-05-01 18:31:51 +0900

    .env ipmlemented

--- first_nest_api/src/app.module.ts
+++ first_nest_api/src/app.module.ts
@@ -5,16 +5,22 @@ import { NominatorPoolsModule } from './nominator_pools/nominator_pools.module';
 import { ExampleModule } from './example/example.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Pool } from './nominator_pools/pool.entity';
-
+import { load } from 'ts-dotenv';
+const env = load({
+    DB_USER: String,
+    DB_PASSWORD: String,
+    DB_HOST: String,
+    DB_NAME: String,
+});
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: env.DB_HOST,
       port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'nominator-pools',
+      username: env.DB_USER,
+      password: env.DB_PASSWORD,
+      database: env.DB_NAME,
       entities: [Pool],
       autoLoadEntities: true,
       synchronize: true, //Indicates if database schema should be auto created on every application launch.
