b2527ae7d07ebd8c222179f40fc88e3d11045609
Hackdady <lenskii.vm19@physics.msu.ru>
2023-06-12 12:56:33 +0900

    created bot with basic functionality

--- telegram_bot_API/index.js
+++ telegram_bot_API/index.js
@@ -1,7 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config()
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+const token = process.env.TELEGRAM_TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
@@ -11,19 +12,34 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
-
   const chatId = msg.chat.id;
   const resp = match[1]; // the captured "whatever"
-
   // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, resp);
+});
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Welcome", {
+  "reply_markup": {
+      "keyboard": [["/sendpic", "/echo Sth"],   ["Hi"], ["I'm robot"]]
+      }
+  });
+  
+  });
+  
+bot.onText(/\/sendpic/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Look at it!");
+  bot.sendPhoto(msg.chat.id,"./Island.jpg", {caption : "Wanna go there? \nSend money to: "} );
 });
 
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-
+  var Hi = "hi";
+  if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+    bot.sendMessage(msg.chat.id,"Hello dear user");
+  }
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  //bot.sendMessage(chatId, 'Received your message, text more!!!');
 });
